b129ff9f3ce7d5f931d16f6eb0d68b9ee95ebc22
Jack Karamanian <karamanian.jack@gmail.com>
2014-01-01 13:07:22 -0600

    Use DeviceType enum for the type field.

--- InputPacket.h
+++ InputPacket.h
@@ -1,14 +1,14 @@
 #pragma once
 #include <cstdint>
+#include "DeviceType.h"
 
 namespace ni {
 	struct InputPacket {
 		constexpr static int MAGIC = 0xD00D;
-		constexpr static int TYPE_LENGTH = 16;
 		constexpr static int DATA_LENGTH = 128;
 
 		uint16_t magic;
-		char type[TYPE_LENGTH];
+		DeviceType type;
 		uint16_t length;
 		uint8_t data[DATA_LENGTH];
 
