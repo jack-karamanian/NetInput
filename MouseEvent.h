35e0ab1f6591592e10dea911cacbd649de4f8da3
Jack Karamanian <karamanian.jack@gmail.com>
2014-01-02 00:58:45 -0600

    Change MouseWheel deltaWheel field size to one byte. Add compile time size check to Mouse Event.

--- MouseEvent.h
+++ MouseEvent.h
@@ -5,9 +5,10 @@ namespace ni {
 	struct MouseEvent {
 		int32_t deltaX;
 		int32_t deltaY;
-		int32_t deltaWheel;
+		int8_t deltaWheel;
 		uint8_t leftButtonDown;
 		uint8_t rightButtonDown;
 		uint8_t middleButtonDown;
 	};
+	static_assert(sizeof(MouseEvent) == 12, "Improper mouse event size");
 }
